06e1753f3c42349e64e84e192e3f48793e54c02d
kutm1 <kublay.tayro@outlook.com>
2025-11-22 18:35:55 -0500

    Fix: Configuración de despliegue y path relativo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/soluciones-gmsl/", // <--- ESTA LÍNEA ES LA CLAVE (nombre exacto de tu repo)
 })
