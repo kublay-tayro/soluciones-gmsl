618d3da3d2af81895a48972731e863bce2c521ed
Kublay Tayro <jktm01@gmail.com>
2026-03-20 14:04:54 -0500

    Configure custom domain soluciones-gmsl.com

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import { AuthProvider } from './contexts/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/soluciones-gmsl">
+    <BrowserRouter>
       <AuthProvider>
         <Routes>
           {/* Tienda pública */}
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/soluciones-gmsl/", // <--- ESTA LÍNEA ES LA CLAVE (nombre exacto de tu repo)
+  base: "/",
 })
